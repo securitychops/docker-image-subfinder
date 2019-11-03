ee0fafb542246c4c69fe6c422c4c39d2baac3328
Security Chops <39027035+securitychops@users.noreply.github.com>
2019-11-03 09:12:30 -0500

    Update .start.sh

--- .start.sh
+++ .start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export PATH=$PATH:/root/go/bin
-
 mkdir -p ~/.aws/
 echo "[default]" >> ~/.aws/credentials
 echo "aws_access_key_id = $S3_BUCKET_KEY" >> ~/.aws/credentials
@@ -9,18 +7,18 @@ echo "aws_secret_access_key = $S3_BUCKET_SECRET" >> ~/.aws/credentials
 
 UUID=$(cat /proc/sys/kernel/random/uuid)
 
-subfinder --set-config VirustotalAPIKey=$VIRUSTOTAL_API_KEY > /dev/null 2>&1
-subfinder --set-config PassivetotalUsername=$PASSIVETOTAL_USERNAME > /dev/null 2>&1
-subfinder --set-config PassivetotalKey=$PASSIVETOTAL_KEY > /dev/null 2>&1
-subfinder --set-config SecurityTrailsKey=$SECURITYTRAILS_KEY > /dev/null 2>&1
-subfinder --set-config RiddlerEmail=$RIDDLER_EMAIL > /dev/null 2>&1
-subfinder --set-config RiddlerPassword=$RIDDLER_PASSWORD > /dev/null 2>&1
-subfinder --set-config CensysUsername=$CENSYS_USERNAME > /dev/null 2>&1
-subfinder --set-config CensysSecret=$CENSYS_SECRET > /dev/null 2>&1
-subfinder --set-config ShodanAPIKey=$SHODAN_API_KEY > /dev/null 2>&1
+./subfinder --set-config VirustotalAPIKey=$VIRUSTOTAL_API_KEY > /dev/null 2>&1
+./subfinder --set-config PassivetotalUsername=$PASSIVETOTAL_USERNAME > /dev/null 2>&1
+./subfinder --set-config PassivetotalKey=$PASSIVETOTAL_KEY > /dev/null 2>&1
+./subfinder --set-config SecurityTrailsKey=$SECURITYTRAILS_KEY > /dev/null 2>&1
+./subfinder --set-config RiddlerEmail=$RIDDLER_EMAIL > /dev/null 2>&1
+./subfinder --set-config RiddlerPassword=$RIDDLER_PASSWORD > /dev/null 2>&1
+./subfinder --set-config CensysUsername=$CENSYS_USERNAME > /dev/null 2>&1
+./subfinder --set-config CensysSecret=$CENSYS_SECRET > /dev/null 2>&1
+./subfinder --set-config ShodanAPIKey=$SHODAN_API_KEY > /dev/null 2>&1
 
 # been having issues with dnsdb and Baidu
-subfinder -v --timeout 10 -d $SCAN_ME -o /tmp/subdomains.txt
+./subfinder -v --timeout 10 -d $SCAN_ME -o /tmp/subdomains.txt
 
 if [ -r /tmp/subdomains.txt ]
 then
