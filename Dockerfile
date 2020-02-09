44d04b5ba2ad09db311554b1b0f558f32046eaf5
chops <me@securitychops.com>
2020-02-09 15:33:45 -0500

    updating to newer alpine image

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,40 @@
-FROM ubuntu:latest
+FROM golang:alpine as builder
 
-# setting maintainer
-LABEL maintainer="@securitychops"
+RUN apk update && \
+    apk add --no-cache git
 
-RUN apt-get update
-RUN apt-get install -y git
-RUN apt-get install -y golang-go
-RUN apt-get install -y python
-RUN apt-get install -y python-pip
-RUN go get github.com/subfinder/subfinder
+WORKDIR /build
 
-RUN pip install awscli
+COPY go-go-gadget.sh .
 
-COPY .start.sh /root/go/bin
+RUN git clone https://github.com/projectdiscovery/subfinder
 
-WORKDIR /root/go/bin
+WORKDIR /build/subfinder/cmd/subfinder
 
-# run our script first yo dawg 
-CMD ["bash", "/root/go/bin/.start.sh"]
+# get all dependencies for subfinder
+RUN go get -d -v
+
+# get rid of all debug info during build
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -ldflags="-w -s" -o /tmp/subfinder
+
+# getting specific to avoid breakages from latest
+FROM alpine:3.11.3
+
+RUN apk update && \
+    apk add --no-cache python && \
+    apk add --no-cache py-pip && \
+    pip install --upgrade awscli && \
+    apk -v --purge del py-pip && \
+    addgroup -S bender && \
+    adduser -S -G bender bender && \
+    chown bender:bender /tmp
+
+COPY --from=builder /build/go-go-gadget.sh /build/subfinder/config.yaml /tmp/subfinder /tmp/
+
+WORKDIR /tmp
+
+USER bender
+
+ENTRYPOINT [ "sh" ]
+
+CMD [ "go-go-gadget.sh" ]
---
+++ go-go-gadget.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+mkdir -p ~/.aws/
+echo "[default]" >> ~/.aws/credentials
+echo "aws_access_key_id = $S3_BUCKET_KEY" >> ~/.aws/credentials
+echo "aws_secret_access_key = $S3_BUCKET_SECRET" >> ~/.aws/credentials
+
+UUID=$(cat /proc/sys/kernel/random/uuid)
+
+if [ ! -z "$BINARYEDGE_API_KEY" ]
+then
+    sed -i "s/binaryedge: \[\]/binaryedge: \\n- $BINARYEDGE_API_KEY/g" config.yaml
+fi
+
+if [ ! -z "$CENSYS_USERNAME" ]
+then
+    if [ ! -z "$CENSYS_SECRET" ]
+    then
+        sed -i "s/censys: \[\]/censys: \\n- $CENSYS_USERNAME:$CENSYS_SECRET/g" config.yaml
+    fi
+fi
+
+if [ ! -z "$CERTSPOTTER_API_KEY" ]
+then
+    sed -i "s/certspotter: \[\]/certspotter: \\n- $CERTSPOTTER_API_KEY/g" config.yaml
+fi
+
+if [ ! -z "$PASSIVETOTAL_USERNAME" ]
+then
+    if [ ! -z "$PASSIVETOTAL_KEY" ]
+    then
+        sed -i "s/passivetotal: \[\]/passivetotal: \\n- $PASSIVETOTAL_USERNAME:$PASSIVETOTAL_KEY/g" config.yaml
+    fi
+fi
+
+if [ ! -z "$SECURITYTRAILS_KEY" ]
+then
+    sed -i "s/securitytrails: \[\]/securitytrails: \\n- $SECURITYTRAILS_KEY/g" config.yaml
+fi
+
+if [ ! -z "$SHODAN_API_KEY" ]
+then
+    sed -i "s/shodan: \[\]/shodan: \\n- $SHODAN_API_KEY/g" config.yaml
+fi
+
+if [ ! -z "$URLSCAN_API_KEY" ]
+then
+    sed -i "s/urlscan: \[\]/urlscan: \\n- $URLSCAN_API_KEY/g" config.yaml
+fi
+
+if [ ! -z "$VIRUSTOTAL_API_KEY" ]
+then
+    sed -i "s/virustotal: \[\]/virustotal: \\n- $VIRUSTOTAL_API_KEY/g" config.yaml
+fi
+
+echo "" >> config.yaml
+
+./subfinder -t 100 -nC --config /tmp/config.yaml -v --timeout 10 -d $SCAN_ME -o /tmp/subdomains.txt
+
+if [ -r /tmp/subdomains.txt ]
+then
+    if [ -s /tmp/subdomains.txt ]
+    then
+        aws s3 mv /tmp/subdomains.txt s3://$S3_BUCKET_NAME/tmp/$UUID
+        echo '{"task_type":"'$NEXT_STEP'","tld":"'$SCAN_ME'","domains_file":"'$UUID'","port_size":"'$PORT_SIZE'"}' > /tmp/$UUID
+        aws s3 mv /tmp/$UUID s3://$S3_BUCKET_NAME/tasks/
+    fi
+fi
